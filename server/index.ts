9f12903816fa33235e68ea8196b09c1fa4265bfe
Roho7 <rohosen2@gmail.com>
2023-09-12 13:18:39 +0530

    mod: backend

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,26 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import cors from "cors";
 import { User } from "./db/database";
+import { SECRET } from "./utils/secret";
+import generateJwt from "./middleware/auth";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-// app.use(cors);
-
-const SECRET = "seCr3T";
 
 mongoose.connect(
   "mongodb+srv://rohosen2:Babla123@cluster0.virj8ol.mongodb.net/",
 );
 
-const generateToken = (user: Object) => {
+const generateToken = <T>(user: T) => {
   const payload = { user };
   return jwt.sign(payload, SECRET, { expiresIn: "1h" });
 };
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
-});
-
 app.post("/signup", async (req: Request, res: Response) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
   const user = await User.findOne({ username });
   if (user) {
     res.json({ message: "Username taken" });
@@ -41,10 +34,21 @@ app.post("/signup", async (req: Request, res: Response) => {
 });
 
 app.post("/signin", async (req: Request, res: Response) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
   const user = await User.findOne({ username });
-  res.send(user);
+  if (password != user?.password) {
+    return res.status(403).json({ error: "password doesn't match" });
+  }
+  const token = generateToken({ username, password });
+  console.log({ username, password });
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  // });
+  return res.json({ message: "successful login", token });
+});
+
+app.get("/", generateJwt, (req: Request, res: Response) => {
+  res.send("Hello");
 });
 
 app.listen(port, () => {
---
+++ server/utils/secret.ts
@@ -0,0 +1,1 @@
+export const SECRET = "seCr3T";
